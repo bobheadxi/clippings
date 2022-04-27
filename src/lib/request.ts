45615397106037d8250f621edba3409c466afab3
Robert Lin <robert@bobheadxi.dev>
2022-04-26 20:40:49 -0700

    deps: bump to obsidian@0.14.6

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,6 +1,6 @@
-import { RequestParam as obsidianRequestParam, request } from 'obsidian';
+import { RequestUrlParam, request } from 'obsidian';
 
-export type RequestParam = obsidianRequestParam;
+export type RequestParam = RequestUrlParam;
 
 export default async function makeRequest(req: RequestParam): Promise<string> {
   return await request(req);
